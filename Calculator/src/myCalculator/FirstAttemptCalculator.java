a3b07f2d3942bdb4aa872e3895dfb856ae257186
Naji Uduigwome-Fredericks <nufredericks@hotmail.co.uk>
2015-10-05 20:24:30 +0100

    Hi Guys this is my first Repo commit codes

---
+++ Calculator/src/myCalculator/FirstAttemptCalculator.java
@@ -0,0 +1,28 @@
+package myCalculator;
+
+
+
+public class FirstAttemptCalculator {
+
+	public static void main(String[] args) {
+		// To write a basic calculator
+		int num1 =5;
+		int num2 = 3;
+		
+		int additionAnswer = num1 + num2;
+		int multiplicationAnswer = num1 * num2;
+		int divisionAnswer = num1 / num2;
+		int subtractionAnswer = num1- num2;
+		int modulnum1num2 =num1%num2;
+		
+		System.out.println(additionAnswer);
+		System.out.println(multiplicationAnswer);
+		System.out.println(divisionAnswer);
+		System.out.println(subtractionAnswer);
+		System.out.println(modulnum1num2);
+		
+		
+
+	}
+
+}
